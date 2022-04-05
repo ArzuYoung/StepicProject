fa7bd0233d16ee3507598cc426cb088ad246732c
Arzu Rzaeva <rzaeva-010203@mail.ru>
2022-04-05 18:29:10 +0500

    add mirror servlet

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -3,19 +3,23 @@ package main;
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.servlet.ServletContextHandler;
 import org.eclipse.jetty.servlet.ServletHolder;
+import servlets.MirrorServlet;
 import servlets.RequestsServlet;
 
 public class Main {
     public static void main(String[] args) throws Exception {
         RequestsServlet requestsServlet = new RequestsServlet();
+        MirrorServlet mirrorServlet = new MirrorServlet();
 
         ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
         context.addServlet(new ServletHolder(requestsServlet), "/*");
+        context.addServlet(new ServletHolder(mirrorServlet), "/mirror");
 
         Server server = new Server(8080);
         server.setHandler(context);
 
         server.start();
+        System.out.println("Server started");
         server.join();
     }
 }
